d190ab968e3735dc9c6e629fe6e877a707e1f176
tamperic <tamaraperic.work@gmail.com>
2025-06-19 14:51:23 +0200

    Import modules from Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClient, HttpClientModule } from "@angular/common/http"; // Simplified API for Angular apps
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 // import { AppRoutigModule } from './app-routing.module';
 import { App } from "./app";
