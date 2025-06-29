514c4963561cabf3a1b8bbd675f462a1ef212574
tamperic <tamaraperic.work@gmail.com>
2025-06-29 17:54:42 +0200

    Update routing & configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { appRoutes } from './app.module';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
-import { importProvidersFrom } from '@angular/core';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(appRoutes), 
+    provideRouter(routes), 
     provideClientHydration(withEventReplay()),
     provideHttpClient(withInterceptorsFromDi()),
     importProvidersFrom(BrowserAnimationsModule, MatDialogModule, MatSnackBarModule)
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-// import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { WelcomePage } from './welcome-page/welcome-page';
+import { MovieCard } from './movie-card/movie-card';
+import { UserProfile } from './user-profile/user-profile';
 
-// export const routes: Routes = [ ];
+export const routes: Routes = [
+    { path: 'welcome', component: WelcomePage },
+    { path: 'movies', component: MovieCard },
+    { path: 'profile', component: UserProfile },
+    { path: '', redirectTo: 'welcome', pathMatch: 'prefix' }
+];
